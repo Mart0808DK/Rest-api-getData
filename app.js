f694d9094036286a05113bfe4ba5f4bb4d5a0b68
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-17 11:16:02 +0200

    get data and dom html

---
+++ app.js
@@ -0,0 +1,57 @@
+"use strict";
+
+window.addEventListener("load", start);
+
+const endpoint = "https://rest-api-startdata-default-rtdb.europe-west1.firebasedatabase.app";
+
+async function start() {
+const getData = await getPosts(`${endpoint}/posts.json`)
+getData.forEach(showData);
+
+console.log(getData);
+
+
+
+
+}
+
+
+async function getPosts(url) {
+const response = await fetch(url)
+
+const data = await response.json()
+const posts = preparePostData(data);
+
+return posts;
+
+}
+
+function preparePostData(dataObject) {
+const postArray = [];
+
+for (const key in dataObject) {
+const post = dataObject[key];
+post.id = key;
+postArray.push(post);
+}
+
+
+return postArray;
+
+}
+
+
+function showData(urlData) {
+    
+const myHtml = /*html*/ `
+<article>
+<p>Id: ${urlData.id}</p>
+<img src=${urlData.image}>
+<p>${urlData.title}</p>
+</article>
+
+`;
+document.querySelector("#displayData").insertAdjacentHTML("beforeend", myHtml);
+
+
+}
