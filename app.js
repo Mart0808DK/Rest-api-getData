f79ba714571df558f9a98d7a6f34b387c1aa25e9
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-18 16:04:07 +0200

    html og js struktur, user fecth and show

--- app.js
+++ app.js
@@ -5,17 +5,29 @@ window.addEventListener("load", start);
 const endpoint = "https://rest-api-startdata-default-rtdb.europe-west1.firebasedatabase.app";
 
 async function start() {
-  const getData = await getPosts(`${endpoint}/posts.json`);
-  getData.forEach(showData);
+  const getPost = await getDataPost(`${endpoint}/posts.json`);
+  const getUser = await getDataUser(`${endpoint}/users.json`);
+  getPost.forEach(showPosts);
+  getUser.forEach(showUser);
 
-  console.log(getData);
+  console.log(getUser);
+  console.log(getPost);
 }
 
-async function getPosts(url) {
+async function getDataPost(url) {
   const response = await fetch(url);
 
   const data = await response.json();
   const posts = preparePostData(data);
+
+  return posts;
+}
+
+async function getDataUser(url) {
+  const response = await fetch(url)
+
+  const data = await response.json()
+  const posts = preparePostData(data)
 
   return posts;
 }
@@ -32,29 +44,44 @@ function preparePostData(dataObject) {
   return postArray;
 }
 
-function showData(urlData) {
+function showPosts(urlData) {
   const myHtml = /*html*/ `
-<article>
-<p>Id: ${urlData.id}</p>
-<img src=${urlData.image}>
-<p>${urlData.title}</p>
-</article>
+  <article>
+    <h3>${urlData.title}</h3>
+    <img src=${urlData.image}>
+    <p>${urlData.body}</p>
+    <p>${urlData.uid}</p>
+  </article>
 
 `;
   document.querySelector("#displayData").insertAdjacentHTML("beforeend", myHtml);
 }
 
-
-async function createPost(title, image) {
-    const newPost = {title, image};
-    const postAsJson = JSON.stringify(newPost);
-    
-    const res = await fetch(`${endpoint}/posts.json`, {
-        method: "POST",
-        body: postAsJson
-    });
-    const data = await res.json();
-    console.log(data)
+function showUser(userData) {
+  const myHtml = /*html*/ `
+  <article>
+    <h3>${userData.name}</h3>
+    <img src=${userData.image}>
+    <p> Title: ${userData.title}</p>
+    <p> Tlf: ${userData.phone}</p>
+    <p> Mail: ${userData.mail}</p>
+  </article>
+  
+  
+  `;
+  document.querySelector("#displayUser").insertAdjacentHTML("beforeend", myHtml);
 }
 
-createPost("My First Post", "https://images.unsplash.com/photo-1641876749963-550554c7258d");
+// async function createPost(title, image) {
+//   const newPost = { title, image };
+//   const postAsJson = JSON.stringify(newPost);
+
+//   const res = await fetch(`${endpoint}/posts.json`, {
+//     method: "POST",
+//     body: postAsJson,
+//   });
+//   const data = await res.json();
+//   console.log(data);
+// }
+
+// // createPost("My First Post", "https://images.unsplash.com/photo-1641876749963-550554c7258d");
